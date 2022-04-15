def58a17b6b07b3e155183f3efa49e5e87d9bf7e
Jan Eickmans <jan.eickmans@hotmail.nl>
2022-04-15 17:17:39 +0200

    Clean started project

---
+++ frontend/pages/components/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from 'next/image'
+import SidebarLink from './SidebarLink'
+import {
+        HomeIcon,
+        HashtagIcon,
+        BellIcon,
+        InboxIcon,
+        BookmarkIcon,
+        ClipboardListIcon,
+        UserIcon,
+        DotsCircleHorizontalIcon
+      } from "@heroicons/react"
+
+function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+      <div className="flex items-center justify-center p-0 w-14 h-14 hoverAnimation xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" alt='' width={30} height={30}/>
+      </div>
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active/>
+        <SidebarLink text="Explore" Icon={HashtagIcon} active/>
+        <SidebarLink text="Notifications" Icon={BellIcon} active/>
+        <SidebarLink text="Messages" Icon={InboxIcon} active/>
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} active/>
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} active/>
+        <SidebarLink text="Profile" Icon={UserIcon} active/>
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} active/>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ frontend/pages/components/SidebarLink.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarLink({Icon, text, active}) {
+  return (
+    <div>
+        <h1>SidebarLink</h1>
+    </div>
+  )
+}
+
+export default SidebarLink
