71f6213d52780c582236226bf1db0ec2da799234
Jan Eickmans <jan.eickmans@hotmail.nl>
2022-04-22 12:43:31 +0200

    Implemented sidebar

--- frontend/pages/components/Sidebar.js
+++ frontend/pages/components/Sidebar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import SidebarLink from './SidebarLink'
+import { HomeIcon } from '@heroicons/react/solid'
 import {
-        HomeIcon,
         HashtagIcon,
         BellIcon,
         InboxIcon,
@@ -10,7 +10,7 @@ import {
         ClipboardListIcon,
         UserIcon,
         DotsCircleHorizontalIcon
-      } from "@heroicons/react"
+      } from "@heroicons/react/outline"
 
 function Sidebar() {
   return (
@@ -27,6 +27,14 @@ function Sidebar() {
         <SidebarLink text="Lists" Icon={ClipboardListIcon} active/>
         <SidebarLink text="Profile" Icon={UserIcon} active/>
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} active/>
+      </div>
+      <buttton className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">Tweet</buttton>
+      <div>
+        <img src="" alt="" className="h-10 w-10 rounded-full xl:mr-2.5" />
+      </div>
+      <div>
+        <h4></h4>
+        <p></p>
       </div>
     </div>
   )
--- frontend/pages/components/SidebarLink.js
+++ frontend/pages/components/SidebarLink.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function SidebarLink({Icon, text, active}) {
   return (
-    <div>
-        <h1>SidebarLink</h1>
+    <div className={'text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation'}>
+        <Icon className="text-white h-7" />
+        <span className="hidden xl:inline">{text}</span>
     </div>
   )
 }
